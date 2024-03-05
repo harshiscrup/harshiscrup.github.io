00124a4ecfeb0319bfb7c994660333367f566ff9
harshiscrup <120416470+harshiscrup@users.noreply.github.com>
2024-03-05 10:09:59 +0530

    Car for boxes

--- Location/index.js
+++ Location/index.js
@@ -2,11 +2,11 @@
 function main() {
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera(80, 2, 0.1, 50000);
-    const renderer = new THREE.WebGLRenderer({ 
-        canvas: document.querySelector('#canvas1') 
+    const renderer = new THREE.WebGLRenderer({
+        canvas: document.querySelector('#canvas1')
     });
 
-    const geom = new THREE.BoxGeometry(20,20,20);
+    // const geom = new THREE.BoxGeometry(20, 20, 20);
 
     const arjs = new THREEx.LocationBased(scene, camera);
 
@@ -20,15 +20,15 @@ function main() {
     let orientationControls;
 
     // Orientation controls only work on mobile device
-    if (isMobile()){   
+    if (isMobile()) {
         orientationControls = new THREEx.DeviceOrientationControls(camera);
-    } 
+    }
 
     let fake = null;
     let first = true;
 
     arjs.on("gpsupdate", pos => {
-        if(first) {
+        if (first) {
             setupObjects(pos.coords.longitude, pos.coords.latitude);
             first = false;
         }
@@ -40,35 +40,35 @@ function main() {
 
     // Uncomment to use a fake GPS location
     //fake = { lat: 51.05, lon : -0.72 };
-    if(fake) {
+    if (fake) {
         arjs.fakeGps(fake.lon, fake.lat);
     } else {
         arjs.startGps();
-    } 
+    }
 
 
     let mousedown = false, lastX = 0;
 
     // Mouse events for testing on desktop machine
-    if(!isMobile()) {
-        window.addEventListener("mousedown", e=> {
+    if (!isMobile()) {
+        window.addEventListener("mousedown", e => {
             mousedown = true;
         });
 
-        window.addEventListener("mouseup", e=> {
+        window.addEventListener("mouseup", e => {
             mousedown = false;
         });
 
-        window.addEventListener("mousemove", e=> {
-            if(!mousedown) return;
-            if(e.clientX < lastX) {
-                camera.rotation.y += mouseStep; 
-                if(camera.rotation.y < 0) {
+        window.addEventListener("mousemove", e => {
+            if (!mousedown) return;
+            if (e.clientX < lastX) {
+                camera.rotation.y += mouseStep;
+                if (camera.rotation.y < 0) {
                     camera.rotation.y += 2 * Math.PI;
                 }
             } else if (e.clientX > lastX) {
                 camera.rotation.y -= mouseStep;
-                if(camera.rotation.y > 2 * Math.PI) {
+                if (camera.rotation.y > 2 * Math.PI) {
                     camera.rotation.y -= 2 * Math.PI;
                 }
             }
@@ -76,17 +76,17 @@ function main() {
         });
     }
 
-	function isMobile() {
-    	if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-        	// true for mobile device
-        	return true;
-    	}
-    	return false;
-	}
+    function isMobile() {
+        if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+            // true for mobile device
+            return true;
+        }
+        return false;
+    }
 
     function render(time) {
         resizeUpdate();
-        if(orientationControls) orientationControls.update();
+        if (orientationControls) orientationControls.update();
         cam.update();
         renderer.render(scene, camera);
         requestAnimationFrame(render);
@@ -95,7 +95,7 @@ function main() {
     function resizeUpdate() {
         const canvas = renderer.domElement;
         const width = canvas.clientWidth, height = canvas.clientHeight;
-        if(width != canvas.width || height != canvas.height) {
+        if (width != canvas.width || height != canvas.height) {
             renderer.setSize(width, height, false);
         }
         camera.aspect = canvas.clientWidth / canvas.clientHeight;
@@ -104,14 +104,26 @@ function main() {
 
     function setupObjects(longitude, latitude) {
         // Use position of first GPS update (fake or real)
-        const material = new THREE.MeshBasicMaterial({color: 0xff0000});
-        const material2 = new THREE.MeshBasicMaterial({color: 0xffff00});
-        const material3 = new THREE.MeshBasicMaterial({color: 0x0000ff});
-        const material4 = new THREE.MeshBasicMaterial({color: 0x00ff00});
-        arjs.add(new THREE.Mesh(geom, material), longitude, latitude + 0.001); // slightly north
-        arjs.add(new THREE.Mesh(geom, material2), longitude, latitude - 0.001); // slightly south
-        arjs.add(new THREE.Mesh(geom, material3), longitude - 0.001, latitude); // slightly west
-        arjs.add(new THREE.Mesh(geom, material4), longitude + 0.001, latitude); // slightly east
+        // const material = new THREE.MeshBasicMaterial({color: 0xff0000});
+        // const material2 = new THREE.MeshBasicMaterial({color: 0xffff00});
+        // const material3 = new THREE.MeshBasicMaterial({color: 0x0000ff});
+        // const material4 = new THREE.MeshBasicMaterial({color: 0x00ff00});
+        const loader = new GLTFLoader();
+        // Load a glTF resource
+        loader.load(
+            // resource URL
+            'assets/McLaren.glb',
+            // called when the resource is loaded
+            function (gltf) {
+
+                arjs.add(new THREE.Mesh(gltf, material), longitude, latitude + 0.001); // slightly north
+                arjs.add(new THREE.Mesh(gltf, material2), longitude, latitude - 0.001); // slightly south
+                arjs.add(new THREE.Mesh(gltf, material3), longitude - 0.001, latitude); // slightly west
+                arjs.add(new THREE.Mesh(gltf, material4), longitude + 0.001, latitude); // slightly east
+
+            },
+        )
+
     }
 
     requestAnimationFrame(render);
