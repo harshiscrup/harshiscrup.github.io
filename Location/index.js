25eeef60991f2b08b2cb1644da935c1524dc12a4
harshiscrup <120416470+harshiscrup@users.noreply.github.com>
2024-03-13 14:01:32 +0530

    Update index.js

--- Location/index.js
+++ Location/index.js
@@ -55,7 +55,7 @@ function main() {
             function (gltf) {
                 car.add(gltf.scene);
                 car.scale.set(5, 5, 5);
-                arjs.add(car, 73.70964976378225, 18.598778400398864, 0.001);
+                arjs.add(car, 73.70964976378225, 18.598778400398864);
                 console.log(car);
             },
         )
