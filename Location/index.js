361a3ad70212544a503e8bba34ff63e40a51658d
harshiscrup <120416470+harshiscrup@users.noreply.github.com>
2024-03-15 14:55:22 +0530

    Update index.js

--- Location/index.js
+++ Location/index.js
@@ -19,7 +19,6 @@ function main() {
             setupObjects(pos.coords.longitude, pos.coords.latitude);
             first = false;
             console.log(pos.coords.longitude, pos.coords.latitude)
-            console.log(pos.coords)
         }
     });
 
