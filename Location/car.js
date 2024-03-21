25ae52e4ec8fe0ae9981848d7415eafabd1d2ef4
harshiscrup <120416470+harshiscrup@users.noreply.github.com>
2024-03-21 13:52:02 +0530

    sa

--- Location/car.js
+++ Location/car.js
@@ -2,32 +2,91 @@
 function main() {
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera(80, 2, 0.1, 50000);
-    const ambientLight = new THREE.AmbientLight(0xffffff, 2);
-    scene.add(ambientLight);
-    const renderer = new THREE.WebGLRenderer({
-        canvas: document.querySelector('#canvas1')
+    const renderer = new THREE.WebGLRenderer({ 
+        canvas: document.querySelector('#canvas1') 
     });
 
-    const arjs = new THREEx.LocationBased(scene, camera, { gpsMinAccuracy : 5});
+    const geom = new THREE.BoxGeometry(20,20,20);
+
+    const arjs = new THREEx.LocationBased(scene, camera);
+
+    // You can change the minimum GPS accuracy needed to register a position - by default 1000m
+    //const arjs = new THREEx.LocationBased(scene, camera. { gpsMinAccuracy: 30 } );
     const cam = new THREEx.WebcamRenderer(renderer, '#video1');
 
-    let orientationControls = new THREEx.DeviceOrientationControls(camera);
+    const mouseStep = THREE.MathUtils.degToRad(5);
 
+
+    let orientationControls;
+
+    // Orientation controls only work on mobile device
+    if (isMobile()){   
+        orientationControls = new THREEx.DeviceOrientationControls(camera);
+    } 
+
+    let fake = null;
     let first = true;
+
     arjs.on("gpsupdate", pos => {
-        if (first) {
+        if(first) {
             setupObjects(pos.coords.longitude, pos.coords.latitude);
             first = false;
-            console.log(pos.coords.longitude, pos.coords.latitude);
         }
     });
 
-    arjs.startGps();
-    // console.log("og")
+    arjs.on("gpserror", code => {
+        alert(`GPS error: code ${code}`);
+    });
+
+    // Uncomment to use a fake GPS location
+    //fake = { lat: 51.05, lon : -0.72 };
+    if(fake) {
+        arjs.fakeGps(fake.lon, fake.lat);
+    } else {
+        arjs.startGps();
+    } 
+
+
+    let mousedown = false, lastX = 0;
+
+    // Mouse events for testing on desktop machine
+    if(!isMobile()) {
+        window.addEventListener("mousedown", e=> {
+            mousedown = true;
+        });
+
+        window.addEventListener("mouseup", e=> {
+            mousedown = false;
+        });
+
+        window.addEventListener("mousemove", e=> {
+            if(!mousedown) return;
+            if(e.clientX < lastX) {
+                camera.rotation.y += mouseStep; 
+                if(camera.rotation.y < 0) {
+                    camera.rotation.y += 2 * Math.PI;
+                }
+            } else if (e.clientX > lastX) {
+                camera.rotation.y -= mouseStep;
+                if(camera.rotation.y > 2 * Math.PI) {
+                    camera.rotation.y -= 2 * Math.PI;
+                }
+            }
+            lastX = e.clientX;
+        });
+    }
+
+	function isMobile() {
+    	if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        	// true for mobile device
+        	return true;
+    	}
+    	return false;
+	}
 
     function render(time) {
         resizeUpdate();
-        if (orientationControls) orientationControls.update();
+        if(orientationControls) orientationControls.update();
         cam.update();
         renderer.render(scene, camera);
         requestAnimationFrame(render);
@@ -36,7 +95,7 @@ function main() {
     function resizeUpdate() {
         const canvas = renderer.domElement;
         const width = canvas.clientWidth, height = canvas.clientHeight;
-        if (width != canvas.width || height != canvas.height) {
+        if(width != canvas.width || height != canvas.height) {
             renderer.setSize(width, height, false);
         }
         camera.aspect = canvas.clientWidth / canvas.clientHeight;
@@ -44,22 +103,17 @@ function main() {
     }
 
     function setupObjects(longitude, latitude) {
-        const loader = new THREE.GLTFLoader();
-        const car = new THREE.Object3D();
-        // Load a glTF resource 
-        loader.load(
-            // resource URL
-            './assets/porsche_gt3_rs/scene.gltf',
-            // called when the resource is loaded
-            function (gltf) {
-                car.add(gltf.scene);
-                car.scale.set(5, 5, 5);
-                arjs.add(car, 73.70964976378225, 18.598778400398864);
-                console.log(car);
-            },
-        )
+        // Use position of first GPS update (fake or real)
+        const material = new THREE.MeshBasicMaterial({color: 0xff0000});
+        const material2 = new THREE.MeshBasicMaterial({color: 0xffff00});
+        const material3 = new THREE.MeshBasicMaterial({color: 0x0000ff});
+        const material4 = new THREE.MeshBasicMaterial({color: 0x00ff00});
+        arjs.add(new THREE.Mesh(geom, material), longitude, latitude + 0.001); // slightly north
+        arjs.add(new THREE.Mesh(geom, material2), longitude, latitude - 0.001); // slightly south
+        arjs.add(new THREE.Mesh(geom, material3), longitude - 0.001, latitude); // slightly west
+        arjs.add(new THREE.Mesh(geom, material4), longitude + 0.001, latitude); // slightly east
     }
-    // console.log(camera);
+
     requestAnimationFrame(render);
 }
 
