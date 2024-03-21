001ceed57600a93da9b0c2ac3601ee3e2aeb1c70
harshiscrup <120416470+harshiscrup@users.noreply.github.com>
2024-03-21 14:02:18 +0530

    Update car.js

--- Location/car.js
+++ Location/car.js
@@ -104,11 +104,18 @@ function main() {
 
     function setupObjects(longitude, latitude) {
         // Use position of first GPS update (fake or real)
-        const material = new THREE.MeshBasicMaterial({color: 0xff0000});
-        const material2 = new THREE.MeshBasicMaterial({color: 0xffff00});
-        const material3 = new THREE.MeshBasicMaterial({color: 0x0000ff});
-        const material4 = new THREE.MeshBasicMaterial({color: 0x00ff00});
-        arjs.add(new THREE.Mesh(geom, material), longitude, latitude + 0.001); // slightly north
+        const loader = new THREE.GLTFLoader();
+        const car = new THREE.Object3D();
+        loader.load(
+            "./assets/porsche_gt3_rs/scene.gltf",
+            function(gltf){
+                car.add(gltf.scene);
+                car.scale.set( 5 , 5 , 5 );
+                arjs.add(car, longitude, latitude + 0.001);
+                console.log(car);
+            }
+        )
+        // arjs.add(new THREE.Mesh(geom, material), longitude, latitude + 0.001); // slightly north
         arjs.add(new THREE.Mesh(geom, material2), longitude, latitude - 0.001); // slightly south
         arjs.add(new THREE.Mesh(geom, material3), longitude - 0.001, latitude); // slightly west
         arjs.add(new THREE.Mesh(geom, material4), longitude + 0.001, latitude); // slightly east
